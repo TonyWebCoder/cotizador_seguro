457985f35369baed5b36f0f17915149d4af98290
TonyWebCoder <132730113+TonyWebCoder@users.noreply.github.com>
2023-07-09 09:39:14 -0500

    estilos terminado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from "react";
 import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+
+import styled from '@emotion/styled';
+
+
+const Contenedor = styled.div`
+  background-color: aqua;
+  max-width: 600px;
+  margin: 0 auto;
+  `;
+
+const ContenedorFormulario = styled.div `
+  background-color: #FFF;
+  padding: 3rem;
+`;
 function App() {
   return (
-    <Header
-    titulo={ ' Cotizador de Seguro'}
-    />
+      <Contenedor>
+            <Header
+              titulo='Cotizador de Seguro'
+            />
+
+            <ContenedorFormulario>
+                <Formulario />
+
+            </ContenedorFormulario>
+      </Contenedor>
   );
 }
 
